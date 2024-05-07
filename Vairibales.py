1ce0789f905e50a4d4990507a28ab73ffc817eae
tyronecass11 <168596265+tyronecass11@users.noreply.github.com>
2024-05-08 10:05:38 +1200

    Add files via upload

---
+++ Vairibales.py
@@ -0,0 +1,10 @@
+# Showing how variables work
+# Tyrone Cass
+# 3 May 2024Year 10 demo
+
+sum = "5" + "5"
+print(sum) 
+
+
+sum = 5 + 5
+print(sum)
---
+++ welcome.py.py
@@ -0,0 +1,11 @@
+#functions go here
+def intro():
+    print("Welcome")
+    print()
+    print("Instructions go here")
+    return "" #I am returning (displaying/using) all items in the code
+
+
+# Main routine
+intro() # Thus calls the function so that we can use it.
+
