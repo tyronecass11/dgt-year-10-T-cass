e2dfedea86883e2dc0b97f5f4fe1404dd1a3d3d5
tyronecass11 <168596265+tyronecass11@users.noreply.github.com>
2024-05-10 14:12:43 +1200

    vairable basics

---
+++ P_02_variables.py
@@ -0,0 +1,14 @@
+# Code to learn about variables...
+
+# Variables can be a string (note the speech marks)
+greeting = "hello world"
+
+# Variables can be a number
+fav_num = 7
+
+# Variables can be Boolean (True / False)
+love_programming = True
+
+print(greeting)
+print(fav_num)
+print(love_programming)
