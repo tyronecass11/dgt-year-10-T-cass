acd0d03307f44e578f40bdf0f1d76e2f899c1aa5
tyronecass11 <168596265+tyronecass11@users.noreply.github.com>
2024-05-10 14:39:54 +1200

    Add files via upload

---
+++ P_03_input_output.py
@@ -0,0 +1,16 @@
+# ask the user for their name 
+username = input("What is your name? ")
+
+#ask the user for their favourite number (integer)
+fav_num = int(input("What is your favourite number? "))
+
+# Double, halve and square the user's favourite number
+double = fav_num * 2
+halve = fav_num / 2
+square = fav_num * fav_num
+print()
+# greet the user
+print(f"Hi {username}, your favourite number is {fav_num}")
+
+# Output the results of doubling, halving and 
+#squaring their favourite integer
